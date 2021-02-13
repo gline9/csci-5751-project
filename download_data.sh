838763055261a9864bb45f04f96c17ca36e02e7f
gline9 <gline9@comcast.net>
2021-02-13 15:27:28 -0600

    Adds data download script

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget -O ./data/reviews.json.gz http://deepyeti.ucsd.edu/jianmo/amazon/categoryFiles/All_Amazon_Review_5.json.gz
+wget -O ./data/metadata.json.gz http://deepyeti.ucsd.edu/jianmo/amazon/metaFiles/All_Amazon_Meta.json.gz
+
