226b8a237e34d7ea036d2a1451b06c148b70a96d
gline9 <gline9@comcast.net>
2021-03-14 19:24:52 -0500

    Adds metadata translation code

--- data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
+++ data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
@@ -1,9 +1,130 @@
 package com.gline9.csci.hbase;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
 
+        Configuration configuration = HBaseConfiguration.create();
+        try (Connection connection = ConnectionFactory.createConnection(configuration))
+        {
+            createTables(connection);
+            putMetadata(connection, args[0]);
+        }
+    }
+
+    private static void putMetadata(Connection connection, String metadataFileName) throws IOException
+    {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        File file = new File(metadataFileName);
+        BufferedReader reader = Files.newBufferedReader(file.toPath());
+
+        Table metadataTable = connection.getTable(TableName.valueOf("metadata"));
+        Table productLinksTable = connection.getTable(TableName.valueOf("productLinks"));
+
+        int lines = 0;
+        List<Put> metadataPuts = new ArrayList<>();
+        List<Put> productLinkPuts = new ArrayList<>();
+        while (reader.ready())
+        {
+            lines++;
+            String line = reader.readLine();
+            MetadataEntry metadataEntry = objectMapper.readValue(line, MetadataEntry.class);
+            metadataPuts.add(metadataEntry.toMetadataPut());
+            productLinkPuts.add(metadataEntry.toProductLinkPut());
+
+            if (lines % 1000 == 0)
+            {
+                printMetadataProgress(lines);
+            }
+
+            if (metadataPuts.size() >= 10000)
+            {
+                metadataTable.put(metadataPuts);
+                metadataPuts.clear();
+                System.out.println("Processed metadata batch: Line " + lines);
+            }
+
+            if (productLinkPuts.size() >= 10000)
+            {
+                productLinksTable.put(productLinkPuts);
+                productLinkPuts.clear();
+                System.out.println("Processed product links batch: Line " + lines);
+            }
+        }
+
+        metadataTable.put(metadataPuts);
+        metadataPuts.clear();
+        System.out.println("Processed metadata batch: Line " + lines);
+
+        productLinksTable.put(productLinkPuts);
+        productLinkPuts.clear();
+        System.out.println("Processed product links batch: Line " + lines);
+
+        System.out.println("Done!");
+    }
+
+    private static void printMetadataProgress(int iterations)
+    {
+        final int totalIterations = 15023059;
+        double percentDone = iterations / (double)totalIterations;
+
+        int barLength = 40;
+        int barProgress = (int)(percentDone * barLength);
+        int barRemaining = barLength - barProgress;
+        String barCharacters = new String(new char[barProgress]).replace("\0", "#");
+        String barFillCharacters = new String(new char[barRemaining]).replace("\0", ".");
+
+        System.out.printf("[%s%s] %.2f%% (%d/%d)%n", barCharacters, barFillCharacters, percentDone * 100, iterations, totalIterations);
+    }
+
+    private static void createTables(Connection connection) throws IOException
+    {
+        Admin admin = connection.getAdmin();
+
+        TableDescriptor reviewsTable = TableDescriptorBuilder.newBuilder(TableName.valueOf("reviews"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("r")).build();
+
+        admin.createTable(reviewsTable);
+
+        TableDescriptor metadataTable = TableDescriptorBuilder.newBuilder(TableName.valueOf("metadata"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("m"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("c"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("o")).build();
+
+        admin.createTable(metadataTable);
+
+        TableDescriptor productLinksTable = TableDescriptorBuilder.newBuilder(TableName.valueOf("productLinks"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("m"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("v"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("b")).build();
+
+        admin.createTable(productLinksTable);
+
+        TableDescriptor overallReviewsTable = TableDescriptorBuilder.newBuilder(TableName.valueOf("overallReviews"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("o")).build();
+
+        admin.createTable(overallReviewsTable);
+
+        TableDescriptor brandReviews = TableDescriptorBuilder.newBuilder(TableName.valueOf("brandReviews"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("o")).build();
+
+        admin.createTable(brandReviews);
     }
 }
--- data-injestor/src/main/java/com/gline9/csci/hbase/MetadataEntry.java
+++ data-injestor/src/main/java/com/gline9/csci/hbase/MetadataEntry.java
@@ -1,13 +1,96 @@
 package com.gline9.csci.hbase;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class MetadataEntry
 {
+    private static final Pattern PRICE_REGEX = Pattern.compile("\\$(?<min>[0-9,]+\\.\\d+)( - \\$(?<max>[0-9,]+\\.\\d+))?");
     private String asin;
     private String price;
     private String brand;
+    private String title;
+    private List<String> category;
+    private List<String> also_view;
+    private List<String> also_buy;
+
+    public Put toMetadataPut()
+    {
+        byte[] metadataFamily = Bytes.toBytes("m");
+        Put put = new Put(Bytes.toBytes(asin));
+        put.addColumn(metadataFamily, Bytes.toBytes("title"), Bytes.toBytes(title));
+        Double price = getPrice();
+        if (null != price)
+        {
+            put.addColumn(metadataFamily, Bytes.toBytes("price"), Bytes.toBytes(price));
+        }
+        put.addColumn(metadataFamily, Bytes.toBytes("brand"), Bytes.toBytes(brand));
+
+        appendListChoiceColumnToPut(put, Bytes.toBytes("c"), category);
+
+        return put;
+    }
+
+    public Put toProductLinkPut()
+    {
+        Put put = new Put(Bytes.toBytes(asin));
+        put.addColumn(Bytes.toBytes("m"), Bytes.toBytes("title"), Bytes.toBytes(title));
+
+        appendListChoiceColumnToPut(put, Bytes.toBytes("v"), also_view);
+        appendListChoiceColumnToPut(put, Bytes.toBytes("b"), also_buy);
+
+        return put;
+    }
+
+    private void appendListChoiceColumnToPut(Put put, byte[] columnFamily, List<String> values)
+    {
+        for (String value : values)
+        {
+            put.addColumn(columnFamily, Bytes.toBytes(value), Bytes.toBytes(true));
+        }
+    }
+
+    public Double getPrice()
+    {
+        if (null == price || price.trim().isEmpty())
+        {
+            return null;
+        }
+
+        Matcher matcher = PRICE_REGEX.matcher(price.trim());
+
+        if (!matcher.matches())
+        {
+            // unknown format, return null
+            return null;
+        }
+
+        Double min = parseDoubleWithCommas(matcher.group("min"));
+        Double ret = min;
+        String maybeMax = matcher.group("max");
+
+        if (null != maybeMax)
+        {
+            Double max = parseDoubleWithCommas(maybeMax);
+            ret = (min + max) / 2;
+        }
+
+        return ret;
+    }
+
+    private Double parseDoubleWithCommas(String string)
+    {
+        return Double.parseDouble(string.replaceAll(",", ""));
+    }
 
     public String toString()
     {
-        return String.format("ASIN: '%s', Price: '%s', Brand: '%s'");
+        return String.format("ASIN: '%s', Price: '%.2f', Brand: '%s', Categories: '%d', Also Viewed: '%d', Also Bought: '%d'", asin, getPrice(), brand, category.size(), also_view.size(), also_buy.size());
     }
 }
