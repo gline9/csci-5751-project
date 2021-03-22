97177c460c2ce6a1fc0fe60224dc9a95d7d09b4e
dulusian <57968437+dulusian@users.noreply.github.com>
2021-03-22 11:48:11 -0500

    review Test

--- data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
+++ data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
@@ -20,10 +20,13 @@ public class Main {
 
         Configuration configuration = HBaseConfiguration.create();
         try (Connection connection = ConnectionFactory.createConnection(configuration)) {
-            countNullValues(connection);
+            //countNullValues(connection);
+            findCorrelation(connection);
+            //findRelationship(connection);
         }
     }
-    public static void countNullValues(Connection connection) throws IOException{
+
+    public static void countNullValues(Connection connection) throws IOException {
         Table reviewTable = connection.getTable(TableName.valueOf("reviews"));
         Table metadataTable = connection.getTable(TableName.valueOf("metadata"));
         Scan ratingScanner = new Scan();
@@ -72,63 +75,76 @@ public class Main {
         int nullBrandCount = 0;
 
 
-
-
         for (Result result = reviewScan.next(); result != null; result = reviewScan.next()) {
             String review = Bytes.toString(result.getValue(ratingFamily, reviewColumn));
-            if(review == "") {
+            if (review == "") {
                 nullReviewCount += 1;
             }
         }
-        System.out.println(nullReviewCount);
+        System.out.println(nullReviewCount); // 0
 
         for (Result result = ratingScan.next(); result != null; result = ratingScan.next()) {
             short rating = Bytes.toShort(result.getValue(ratingFamily, ratingColumn));
-            if(rating == 0) {
+            if (rating == 0) {
                 nullRatingCount += 1;
             }
         }
 
-        System.out.println(nullRatingCount);
+        System.out.println(nullRatingCount); //3
         for (Result result = reviewerIDScan.next(); result != null; result = reviewerIDScan.next()) {
             String reviewerID = Bytes.toString(result.getValue(ratingFamily, reviewerIDColumn));
-            if(reviewerID == "") {
+            if (reviewerID == "") {
                 nullReviewerIDCount += 1;
             }
         }
-        System.out.println(nullReviewerIDCount);
+        System.out.println(nullReviewerIDCount); //0
 
         for (Result result = titleScan.next(); result != null; result = titleScan.next()) {
             String title = Bytes.toString(result.getValue(metadataFamily, titleColumn));
-            if(title == "") {
+            if (title == "") {
                 nullTitleCount += 1;
             }
         }
-        System.out.println(nullTitleCount);
+        System.out.println(nullTitleCount);//4365
 
         for (Result result = priceScan.next(); result != null; result = priceScan.next()) {
             double price = Bytes.toDouble(result.getValue(metadataFamily, priceColumn));
-            if(price == 0) {
+            if (price == 0) {
                 nullPriceCount += 1;
             }
         }
-        System.out.println(nullPriceCount);
+        System.out.println(nullPriceCount); //129
 
         for (Result result = brandScan.next(); result != null; result = brandScan.next()) {
             String brand = Bytes.toString(result.getValue(metadataFamily, brandColumn));
-            if(brand == "") {
+            if (brand == "") {
                 nullBrandCount += 1;
             }
         }
-        System.out.println(nullBrandCount);
+        System.out.println(nullBrandCount); //1411951
 
         for (Result result = summaryScan.next(); result != null; result = summaryScan.next()) {
             String summary = Bytes.toString(result.getValue(metadataFamily, summaryColumn));
-            if(summary == "") {
+            if (summary == "") {
                 nullSummaryCount += 1;
             }
         }
-        System.out.println(nullSummaryCount);
+        System.out.println(nullSummaryCount); //0
     }
 
+    public static void findCorrelation(Connection connection) throws IOException {
+        Table reviewTable = connection.getTable(TableName.valueOf("reviews"));
+        //Table metadataTable = connection.getTable(TableName.valueOf("metadata"));
+        Scan ratingScanner = new Scan();
+        byte[] ratingFamily = Bytes.toBytes("r");
+        byte[] ratingColumn = Bytes.toBytes("rating");
+        ResultScanner ratingScan = reviewTable.getScanner(ratingScanner);
+        for (Result result = ratingScan.next(); result != null; result = ratingScan.next()) {
+            System.out.println(result);
+        }
+    }
+
+    public static void findRelationship(Connection connection) throws IOException {
+
+    }
 }
