80311807bba0d52e09554eb3eba8c108f4d0a0c1
dulusian <57968437+dulusian@users.noreply.github.com>
2021-03-23 16:05:48 -0500

    Relationship function update

--- data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
+++ data-injestor/src/main/java/com/gline9/csci/hbase/Main.java
@@ -87,10 +87,7 @@ public class Main {
 //                System.out.println(entry.getKey());
 //                System.out.println(priceColumn);
 //                System.out.println(result.getValue(reviewFamily, entry.getKey()));
-                if (result.getValue(metadataFamily, priceColumn) != null){
-                    System.out.println("Hello world");
-                }
-                if (result.getValue(metadataFamily, priceColumn) != null && result.getValue(overallFamily, result.getValue(metadataFamily, entry.getKey())) != null) {
+                if (result.getValue(metadataFamily, priceColumn) != null && result.getValue(overallFamily, entry.getKey()) != null) {
                     System.out.println("We are in");
                     if (previousKey == metadataKey) {
                         price = Bytes.toDouble(result.getValue(metadataFamily, priceColumn));
